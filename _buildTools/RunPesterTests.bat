4feb423aaa7543b3c7700f464ac40dec79c85202
Marcin Grzywa <mgrzywa@objectivity.co.uk>
2015-02-15 17:12:11 +0100

    RunPesterTests - 64-bit powershell and new nunit xml format.

--- _buildTools/RunPesterTests.bat
+++ _buildTools/RunPesterTests.bat
@@ -3,6 +3,12 @@ SET ARGS=%*
 if NOT '%1'=='' SET ARGS=%ARGS:"=\"% 
 if '%1'=='' SET ARGS=-Tag PSCI.unit,PSCI.integration -Path %~dp0..
 
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -NonInteractive -NoProfile -ExecutionPolicy Bypass -Command ^
+if exist { %systemroot%\sysnative } ( 
+   set pspath=%systemroot%\sysnative\WindowsPowerShell\v1.0\powershell.exe
+) else (
+   set pspath=%systemroot%\system32\WindowsPowerShell\v1.0\powershell.exe
+)
+%pspath% -NonInteractive -NoProfile -ExecutionPolicy Bypass -Command ^
  "& { Import-Module '%~dp0\Pester\Pester.psm1'; Invoke-Pester -OutputFile Test.xml -OutputFormat NUnitXml -Strict -EnableExit %ARGS%}"
 if '%TEAMCITY_VERSION%'=='' pause
+exit /B %errorlevel%
