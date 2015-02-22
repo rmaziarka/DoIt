6e60d9e0a0e00758fd913614a673c7c1f251068b
Marcin Grzywa <mgrzywa@objectivity.co.uk>
2015-02-22 22:52:27 +0100

    Excluded 'examples' from ScriptCop.

--- _buildTools/RunScriptCop.bat
+++ _buildTools/RunScriptCop.bat
@@ -4,7 +4,7 @@ SET SCRIPTCOP_PATH=%1
 SET SCRIPTCOP_EXCLUDE=%2
 SET SCRIPTCOP_PREREQ=%3
 IF "%1"=="" SET SCRIPTCOP_PATH=%~dp0..
-IF "%2"=="" SET SCRIPTCOP_EXCLUDE="@('bat','_buildTools','dsc','externalLibs','teamcityProvisioning')"
+IF "%2"=="" SET SCRIPTCOP_EXCLUDE="@('bat','_buildTools','dsc','externalLibs','teamcityProvisioning','examples')"
 IF "%3"=="" SET SCRIPTCOP_PREREQ=%~dp0..\PSCI.classes.ps1,%~dp0..\core\utils\Write-ProgressExternal.ps1
 )
 powershell -command "& {. '.\Invoke-ScriptCop.ps1'; Invoke-ScriptCop -Path '%SCRIPTCOP_PATH%' -ExcludeNames %SCRIPTCOP_EXCLUDE% -PrerequisitesPaths %SCRIPTCOP_PREREQ% }"
