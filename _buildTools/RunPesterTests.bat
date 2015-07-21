01d62f02fa29454a255e751924a51174059cbf24
Marcin Grzywa <mgrzywa@objectivity.co.uk>
2015-07-21 16:39:07 +0200

    Fixed build tools bats.

--- _buildTools/RunPesterTests.bat
+++ _buildTools/RunPesterTests.bat
@@ -5,5 +5,5 @@ if '%1'=='' SET ARGS=-Tag PSCI.unit,PSCI.integration -Path %~dp0..
 
 powershell -NonInteractive -NoProfile -ExecutionPolicy Bypass -Command ^
  "& { Import-Module '%~dp0\Pester\Pester.psm1'; Invoke-Pester -OutputFile Test.xml -OutputFormat NUnitXml -Strict -EnableExit %ARGS%}"
-if '%TEAMCITY_VERSION%'=='' pause
+if "%TEAMCITY_VERSION%"=="" pause
 exit /B %errorlevel%
--- _buildTools/RunScriptCop.bat
+++ _buildTools/RunScriptCop.bat
@@ -6,7 +6,7 @@ IF "%1"=="" SET SCRIPTCOP_PATH=%~dp0..
 IF "%2"=="" SET SCRIPTCOP_EXCLUDE="@('bat','_buildTools','dsc','externalLibs','teamcityProvisioning','examples')"
 
 powershell -command "& {. '.\Invoke-ScriptCop.ps1'; Invoke-ScriptCop -Path '%SCRIPTCOP_PATH%' -ExcludeNames %SCRIPTCOP_EXCLUDE% }"
-if '%TEAMCITY_VERSION%'=='' pause
+if "%TEAMCITY_VERSION%"=="" pause
 EXIT /B %errorlevel%
 
 :parametermissing
