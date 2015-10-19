79bed91f45077a6d8b3f7c060590d365da868032
Marcin Grzywa <mgrzywa@objectivity.co.uk>
2015-10-19 16:34:46 +0200

    Updated PSCIScheduledTask.

--- modules/deploy/BuiltinSteps/PSCIScheduledTask.ps1
+++ modules/deploy/BuiltinSteps/PSCIScheduledTask.ps1
@@ -24,41 +24,104 @@ SOFTWARE.
 
 configuration PSCIScheduledTask {
 
-    param ($NodeName, $Tokens) 
+    <#
+    .SYNOPSIS
+    Ensures specific Scheduled Tasks are configured in Task Scheduler.
+
+    .DESCRIPTION
+    It uses following common tokens:
+    - **TaskName** - (Mandatory) Scheduled Task name
+    - **PowershellCommandToRun** - powershell command to run (puts powershell.exe to Execute and command to Argument)
+    - **Execute** - path to executable to run
+    - **Argument** - argument for executable to run
+    - **WorkingDirectory** - working directory
+    - **Credential** - credential under which the Scheduled Task will run (if not specified, SYSTEM will be used)
+    - **RunLevel** - Highest (administrator - default) or Limited
+    - **Compatibility** - At, V1, Vista, Win7 or Win8 (default)
+    - **Description** - description of the Scheduled Task
+    - **TaskPath** - path where the task will be created in Scheduled Task tree. 
+
+    And following tokens for settings the schedule:
+    - **ScheduledAt** - first time when the task should run (datetime)
+    - **Daily** - whether to run it daily (true/false)
+
+    Or:
+    - **ScheduledAt** - first time when the task should run (datetime)
+    - **ScheduledTimeSpanDay** - run it every n days
+    - **ScheduledTimeSpanHour** - run it every n hours
+    - **ScheduledTimeSpanMin** - run it every n minutes
+    
+    See [https://github.com/guitarrapc/DSCResources/tree/master/Custom/GraniResource/DSCResources/Grani_ScheduleTask] for description of DSC resource used in this step.
+
+    .EXAMPLE
+    ```
+    Import-Module "$PSScriptRoot\..\PSCI\PSCI.psd1" -Force
+
+    Environment Local { 
+        ServerConnection WebServer -Nodes localhost
+        ServerRole Web -Steps 'PSCIScheduledTask' -ServerConnection WebServer
+
+        Tokens Web @{
+            ScheduledTasks = @{
+                TaskName = 'MyTask'
+                Credential = { ConvertTo-PSCredential -User $($Tokens.Credentials.ScheduledTaskUser) -Password $Tokens.Credentials.ScheduledTaskPassword }
+                PowershellCommandToRun = @(
+                    'Set-Content -Path "c:\test.txt" -Value "test" -Force',
+                    'Write-Host "test"'
+                    ) -join ' '
+                ScheduledAt = [datetime]'2015-08-25 20:00:00'
+                ScheduledTimeSpanDay = 0
+                ScheduledTimeSpanHour = 0
+                ScheduledTimeSpanMin = 15
+            }
+        }
+    }
+
+    Install-DscResources -ModuleNames GraniResource
+
+    Start-Deployment -Environment Local -NoConfigFiles
+    ```
+    Installs specified scheduled task.
+
+    #>
 
     Import-DscResource -ModuleName GraniResource
 
-    if (!$Tokens.ScheduledTasks) {
-        return
-    }
+    Node $AllNodes.NodeName {
 
-    Node $NodeName {
+        $scheduledTasks = Get-TokenValue -Name 'ScheduledTasks'
 
-        foreach ($task in $Tokens.ScheduledTasks.GetEnumerator()) {
-            $taskName = $task.Key
-            $taskSettings = $task.Value
+        if (!$scheduledTasks) {
+            Write-Log -Info "No scheduled tasks to deploy"
+            return
+        }
 
-            if ($taskSettings.PowershellCommandToRun) {
-                $taskSettings.Execute = 'powershell.exe'
-                $taskSettings.Argument = '-Command "{0}"' -f $taskSettings.PowershellCommandToRun
+        foreach ($task in $scheduledTasks) {
+            if ($task -isnot [hashtable]) {
+                throw "ScheduledTasks token must be a hashtable or array of hashtables."
             }
 
-            if ($taskSettings.ScheduledTimeSpanDay -or $taskSettings.ScheduledTimeSpanHour -or $taskSettings.ScheduledTimeSpanMin) { 
+            if ($task.PowershellCommandToRun) {
+                $task.Execute = 'powershell.exe'
+                $task.Argument = '-Command "{0}"' -f $task.PowershellCommandToRun
+            }
+
+            if ($task.ScheduledTimeSpanDay -or $task.ScheduledTimeSpanHour -or $task.ScheduledTimeSpanMin) { 
                 cScheduleTask "TaskSchedule_$taskName" {
                     Ensure = 'Present'
-                    TaskName = $taskSettings.TaskName
-                    TaskPath = '\'
-                    Description = $taskSettings.Description
-                    Execute = $taskSettings.Execute
-                    Argument = $taskSettings.Argument
-                    WorkingDirectory = $taskSettings.WorkingDirectory
-                    Credential = $taskSettings.Credential
-                    RunLevel = if ($taskSettings.RunLevel) { $taskSettings.RunLevel } else { 'Highest' }
-                    Compatibility = if ($taskSettings.Compatibility) { $taskSettings.Compatibility } else { 'Win8' }
-                    ScheduledAt = $taskSettings.ScheduledAt
-                    ScheduledTimeSpanDay = $taskSettings.ScheduledTimeSpanDay
-                    ScheduledTimeSpanHour = $taskSettings.ScheduledTimeSpanHour
-                    ScheduledTimeSpanMin = $taskSettings.ScheduledTimeSpanMin
+                    TaskName = $task.TaskName
+                    TaskPath = if ($task.TaskPath) { $task.TaskPath } else { '\' }
+                    Description = $task.Description
+                    Execute = $task.Execute
+                    Argument = $task.Argument
+                    WorkingDirectory = $task.WorkingDirectory
+                    Credential = $task.Credential
+                    RunLevel = if ($task.RunLevel) { $task.RunLevel } else { 'Highest' }
+                    Compatibility = if ($task.Compatibility) { $task.Compatibility } else { 'Win8' }
+                    ScheduledAt = $task.ScheduledAt
+                    ScheduledTimeSpanDay = $task.ScheduledTimeSpanDay
+                    ScheduledTimeSpanHour = $task.ScheduledTimeSpanHour
+                    ScheduledTimeSpanMin = $task.ScheduledTimeSpanMin
                     ScheduledDurationDay = 0
                     ScheduledDurationHour = 0
                     ScheduledDurationMin = 0
@@ -69,24 +132,21 @@ configuration PSCIScheduledTask {
             } else {
                 cScheduleTask "TaskSchedule_$taskName" {
                     Ensure = 'Present'
-                    TaskName = $taskSettings.TaskName
-                    TaskPath = '\'
-                    Description = $taskSettings.Description
-                    Execute = $taskSettings.Execute
-                    Argument = $taskSettings.Argument
-                    WorkingDirectory = $taskSettings.WorkingDirectory
-                    Credential = $taskSettings.Credential
-                    RunLevel = if ($taskSettings.RunLevel) { $taskSettings.RunLevel } else { 'Highest' }
-                    Compatibility = if ($taskSettings.Compatibility) { $taskSettings.Compatibility } else { 'Win8' }
-                    ScheduledAt = $taskSettings.ScheduledAt
-                    Daily = $taskSettings.Daily
+                    TaskName = $task.TaskName
+                    TaskPath = if ($task.TaskPath) { $task.TaskPath } else { '\' }
+                    Description = $task.Description
+                    Execute = $task.Execute
+                    Argument = $task.Argument
+                    WorkingDirectory = $task.WorkingDirectory
+                    Credential = $task.Credential
+                    RunLevel = if ($task.RunLevel) { $task.RunLevel } else { 'Highest' }
+                    Compatibility = if ($task.Compatibility) { $task.Compatibility } else { 'Win8' }
+                    ScheduledAt = $task.ScheduledAt
+                    Daily = $task.Daily
                     Disable = $false
                     Hidden = $false               
                 }
             }
-
         }
     }
-
-    
 }
