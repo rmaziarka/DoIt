a44990c171f350915ad4888caf2f86fe0d39d4e3
Marcin Grzywa <mgrzywa@objectivity.co.uk>
2015-10-22 09:14:14 +0200

    Excluded cCertificate from license checks.

--- _buildTools/sanity_checks.ps1
+++ _buildTools/sanity_checks.ps1
@@ -50,7 +50,7 @@ SOFTWARE.
 
 $customSources = Get-ChildItem -Path "$PSScriptRoot\.." -File -Filter "*.ps*1" -Recurse | 
                  Select-Object -ExpandProperty FullName |
-                 Where-Object { $_ -inotmatch '(_buildTools\\Pester|_buildtools\\ScriptCop|externalLibs\\|dsc\\ext|cIIS\\|PSHOrg|examples\\|OBJ_cServiceResource)' }
+                 Where-Object { $_ -inotmatch '(_buildTools\\Pester|_buildtools\\ScriptCop|externalLibs\\|dsc\\ext|cIIS\\|PSHOrg|examples\\|OBJ_cServiceResource|cCertificate\\)' }
 
 
 $licenseNotMatching = New-Object -TypeName System.Collections.ArrayList
